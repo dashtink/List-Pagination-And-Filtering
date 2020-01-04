cc6d8b104c5cc92205d789f72dde086aa78e88b3
Danielle Kellogg <dashkellogg@gmail.com>
2020-01-04 11:09:42 -0500

    Clean up
    Cleans up comments, updates variable names to be more specific

--- js/script.js
+++ js/script.js
@@ -1,50 +1,28 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Dani Kellogg 2020
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   Global Variables
 ***/
 
 let studentList = document.getElementsByClassName('student-item');
 let itemsPerPage = 10;
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   Function to conditionally display students on page
 ***/
 
-function showPage (list, page) {
+const showPage = (list, page) => {
    let startIndex = (page * itemsPerPage) - itemsPerPage;
    let endIndex = page * itemsPerPage;
 
    for (i = 0; i < list.length; i++){
       if (i >= startIndex && i <= endIndex){
          list[i].style.display = '';
-         console.log(list[i]);
       }
-
       else {
          list[i].style.display = 'none';
       }
@@ -54,48 +32,42 @@ function showPage (list, page) {
 showPage(studentList,1);
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   Function to append the pagination links at the top of the student list
 ***/
 
-function appendPageLinks (list) {
+const appendPageLinks = (list) => {
+   let numberOfLinks = Math.ceil(list.length / itemsPerPage);
    let div = document.createElement('div');
    div.className = 'pagination';
-   let link = document.createElement('ul');
+   let unorderedList = document.createElement('ul');
+   div.appendChild(unorderedList);
    let pageParent = document.querySelector('.page');
-   let pageList = document.querySelector('.page-header');
-   let numberButtons = Math.ceil(list.length / itemsPerPage);
+   let pageHeader = document.querySelector('.page-header');
 
-   for (let i = 0; i < numberButtons; i++){
-   let lineItem = document.createElement('li');
-   lineItem.innerHTML = '<a href=#>' + i + '</a>';
-   //* Each LI element should contain an A element with an href attribute of #, and text set to the page number each link will show. First link is 1. Second link is 2. And so on.*/
-      
-   //* Add the active class name to the first pagination link initially.*/
-
-   //* Add a “click” event listener to each A element. A loop can be helpful here.*/
-
-   link.appendChild(lineItem);
-
-   let links = lineItem.children;
-
-   for (let i = 0; i < links.length; i++){
-   links[i].addEventListener('click', (e) => {
-      console.log('link clicked');
-   });
-   };
-  
-
+   /** Create line items and insert anchor tags inside ul. */
+   for (let i = 1; i < numberOfLinks; i++){
+      let lineItem = document.createElement('li');
+      lineItem.innerHTML = '<a href=#>' + i + '</a>';
+      unorderedList.appendChild(lineItem);
    };
 
-   div.appendChild(link);
-   pageParent.insertBefore(div, pageList);
+   //* Add a “click” event listener to each anchor tag. */
+   let links = document.getElementsByTagName('A');
+   console.log(links);
 
-   console.log(list);
+   for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", (event) => {
+         for (let i = 0; i < links.length; i++){
+         links[i].classList.remove('active');
+         };
+         event.target.classList.add('active');
+         
+         console.log('clicked');
+      });    
+   };
+   
+   pageParent.insertBefore(div, pageHeader);
 
 }
+
 appendPageLinks(studentList);
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
